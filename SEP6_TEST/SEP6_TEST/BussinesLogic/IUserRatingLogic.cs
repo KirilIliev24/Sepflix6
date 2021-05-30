9bf7cf84fa37e2353976a00c5579c5360dbb9e8e
HarryTomovski <43003754+HarryTomovski@users.noreply.github.com>
2021-05-30 15:15:55 +0200

    Implementation of DbAccess and Logic for UserRating

---
+++ SEP6_TEST/SEP6_TEST/BussinesLogic/IUserRatingLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEP6_TEST.BussinesLogic
+{
+    public interface IUserRatingLogic
+    {
+        public bool addUserRating(string username, int movieId, int ratingGiven);
+
+        public int getUserRating(string username, int movieId);
+    }
+}
---
+++ SEP6_TEST/SEP6_TEST/DbAccess/IUserRatingDb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEP6_TEST.DbAccess
+{
+    public interface IUserRatingDb
+    {
+
+        public void saveUserRating(string username, int movieId,int ratingGiven);
+
+        public bool hasUserVoted(string username, int movieId);
+
+        public int returnUserRating(string username, int movieId);
+
+        
+    }
+}
